aaffa4d55a84d084b46e8286dc003247ab3d459b
Ashfaque-03 <abdulashfaque0367@gmail.com>
2023-02-26 13:46:45 +0530

    last changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -11,8 +11,6 @@ function App() {
       {/* <img src="/favicon.png" alt="" /> */}
         <div className="App-header">
           <Router>
-            
-            <h1></h1>
             <Routes>
               <Route exact path="/" element={<HomeComponent/>}/>
               <Route exact path="/student/add" element={<StdFormComponent/>}/>
